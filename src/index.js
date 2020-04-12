7c5f84eb3a04fb5b8c5dd3224edf2383f0af0761
Hardelele <Hardelele@yahoo.com>
2020-04-12 21:36:07 +0300

    Add redux store

--- src/index.js
+++ src/index.js
@@ -3,10 +3,26 @@ import ReactDOM from 'react-dom';
 import './css/index.css';
 import * as serviceWorker from './serviceWorker';
 import Application from "./Application";
+import {applyMiddleware, compose, createStore} from "redux";
+import thunk from "redux-thunk";
+import {Provider} from "react-redux";
+import {rootReducer} from "./redux/reducers/rootReducer";
+
+const store = createStore(rootReducer, compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+));
+
+const app = () => {
+  return (
+      <Provider store={store}>
+          <Application/>
+      </Provider>
+  );
+};
 
 ReactDOM.render(
-    <Application/>,
-    document.getElementById('root')
+    app(), document.getElementById('root')
 );
 
 serviceWorker.unregister();
