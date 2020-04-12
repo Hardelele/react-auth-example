82e37bfb91c45bd4d00350c829fc0a4509012951
Hardelele <Hardelele@yahoo.com>
2020-04-12 15:20:04 +0300

    init app

---
+++ src/Application.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './css/App.css';
+
+function Application() {
+  return (
+    <div className="application-container">
+    </div>
+  );
+}
+
+export default Application;
