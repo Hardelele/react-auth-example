518ccaf6b0d6fe6e7697053fc4ef6d068375661b
Hardelele <Hardelele@yahoo.com>
2020-04-12 21:35:51 +0300

    Route path="/profile"

--- src/Application.js
+++ src/Application.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 import './css/application.css';
 import {Authorization} from "./components/Authorization";
+import {Profile} from "./components/Profile";
 
 function Application() {
   return (
@@ -9,6 +10,7 @@ function Application() {
         <div className="application-container">
           <Switch>
             <Route path="/" exact component={Authorization}/>
+            <Route path="/profile" component={Profile}/>
           </Switch>
         </div>
       </BrowserRouter>
