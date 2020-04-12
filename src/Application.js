db19229a048ff553d82426e11be2de7436f91c5b
Hardelele <Hardelele@yahoo.com>
2020-04-12 19:50:07 +0300

    Add BrowserRouter and Route to Authorization

--- src/Application.js
+++ src/Application.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import './css/App.css';
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import './css/application.css';
+import {Authorization} from "./components/Authorization";
 
 function Application() {
   return (
-    <div className="application-container">
-    </div>
+      <BrowserRouter>
+        <div className="application-container">
+          <Switch>
+            <Route path="/" exact component={Authorization}/>
+          </Switch>
+        </div>
+      </BrowserRouter>
   );
 }
 
