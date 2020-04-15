b0c92bb880c03f0f27fa4796e9159e3a777cb1e1
Hardelele <Hardelele@yahoo.com>
2020-04-15 16:53:08 +0300

    Add isAuth field and INVALIDATE_AUTH_ACTION

--- src/redux/reducers/tokensReducer.js
+++ src/redux/reducers/tokensReducer.js
@@ -1,16 +1,19 @@
-import {AUTH_ACTION, REGISTER_ACTION} from "../constants/Actions";
+import {AUTH_ACTION, INVALIDATE_AUTH_ACTION, REGISTER_ACTION} from "../constants/Actions";
 
 const initialState = {
     tokens: [],
+    isAuth: false
 };
 
 export function tokensReducer(state = initialState, action) {
     console.log(action);
     switch (action.type) {
         case AUTH_ACTION:
-            return {...state, ...{tokens: action.payload}};
+            return {...state, ...{tokens: action.payload, isAuth: true}};
         case REGISTER_ACTION:
-            return {...state, ...{tokens: action.payload}};
+            return {...state, ...{tokens: action.payload, isAuth: true}};
+        case INVALIDATE_AUTH_ACTION:
+            return {...state, ...initialState};
         default:
             return state;
     }
