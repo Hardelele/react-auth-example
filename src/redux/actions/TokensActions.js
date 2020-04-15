0545a0556891054808333729e122e5c6c6827c46
Hardelele <Hardelele@yahoo.com>
2020-04-15 16:52:03 +0300

    Add invalidateAuth

--- src/redux/actions/TokensActions.js
+++ src/redux/actions/TokensActions.js
@@ -1,14 +1,17 @@
 import axios from "axios";
 import {POST_AUTH, POST_REGISTER} from "../constants/Endpoints";
-import {AUTH_ACTION, REGISTER_ACTION} from "../constants/Actions";
+import {AUTH_ACTION, INVALIDATE_AUTH_ACTION, REGISTER_ACTION} from "../constants/Actions";
 
 const saveTokens = (data) => {
     window.localStorage.setItem("access_token", data.access_token);
     window.localStorage.setItem("refresh_token", data.refresh_token);
 };
 
-const clearLocalStorage = (ignore) => {
+const invalidateAuth = (ignore) => {
     window.localStorage.clear();
+    return dispatch => {
+        dispatch({type: INVALIDATE_AUTH_ACTION})
+    };
 };
 
 export function postAuth(authInfo) {
@@ -16,15 +19,15 @@ export function postAuth(authInfo) {
         axios.post(POST_AUTH, authInfo).then(response => {
             saveTokens(response.data);
             dispatch({type: AUTH_ACTION, payload: response.data});
-        }).catch(clearLocalStorage);
+        }).catch(invalidateAuth);
     }
 }
 
-export function postRegister(registerInfo) {
+export async function postRegister(registerInfo) {
     return dispatch => {
         axios.post(POST_REGISTER, registerInfo).then(response => {
                 saveTokens(response.data);
                 dispatch({type: REGISTER_ACTION, payload: response.data});
-        }).catch(clearLocalStorage);
+        }).catch(invalidateAuth);
     }
 }
